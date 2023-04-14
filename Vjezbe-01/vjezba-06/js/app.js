b5c2aaa37445657eba91ae609d2cac3df0b5d6e5
razvigor <kamaradsasa@gmail.com>
2023-04-14 21:14:47 +0200

    style

---
+++ Vjezbe-01/vjezba-06/js/app.js
@@ -0,0 +1,29 @@
+function Box({ style, className = '', ...otherProps }) {
+	return (
+		<div
+			className={`box ${className}`}
+			style={{ fontStyle: 'italic', ...style }}
+			{...otherProps}
+		/>
+	);
+}
+
+function App() {
+	return (
+		<section className='boxes'>
+			<Box className='box--small' style={{ backgroundColor: 'green' }}>
+				Small green box
+			</Box>
+			<Box className='box--medium' style={{ backgroundColor: 'orange' }}>
+				Middle orange box
+			</Box>
+			<Box className='box--large' style={{ backgroundColor: 'blue' }}>
+				Big blue box
+			</Box>
+			<Box>Box without properties</Box>
+		</section>
+	);
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
