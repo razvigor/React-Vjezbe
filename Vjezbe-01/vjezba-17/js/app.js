5859983844591ffcb9d9bda9a8fbc95ecb833eac
razvigor <kamaradsasa@gmail.com>
2023-05-03 19:15:50 +0200

    useContext and useReducer together

--- Vjezbe-01/vjezba-17/js/app.js
+++ Vjezbe-01/vjezba-17/js/app.js
@@ -25,7 +25,7 @@ function countReducer(state, action) {
 function useCountContext() {
 	const context = React.useContext(CountContext);
 	if (!context) {
-		throw new Error('useCount must be used within the CountProvider');
+		throw new Error('useCountContext must be used within the CountProvider');
 	}
 	return context;
 }
